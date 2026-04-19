820d0de827b82b0863a33518c44269bc673c8ad0
Mohamed Mahmodi <mohammed5mmc@gmail.com>
2026-04-19 12:53:24 +0300

    first section done

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,7 +1,16 @@
-export default function Button({ children, className }: { children: React.ReactNode; className?: string }) {
+export default function Button({ children, className, colors }: { children: React.ReactNode; className?: string; colors?: string; }) {
     return (
-        <button className={`cursor-pointer text-white text-lg bg-primary/85 py-3 px-5 border-l-white/35 border-t-white/35 border-b-white/25 border-r-white/25 border-4 text-shadow-primary text-shadow-[0_2px_red] shadow-primary shadow-[0_4px_0_0] transition-all duration-150 ease-in-out active:shadow-[0_0px_0_0] active:translate-y-1 ${className || ''}`}>
-          {children}
+        <button className={`
+            cursor-pointer py-3 px-5 text-white text-lg border-4
+            text-shadow-[0_2px] shadow-[0_4px_0_0]
+            ${colors || 'bg-primary/85 text-shadow-primary shadow-primary'}
+            border-l-white/35 border-t-white/35
+            border-b-white/25 border-r-white/25
+            transition-all duration-150 ease-in-out
+            active:shadow-[0_0px_0_0] active:translate-y-1
+            ${className || ''}
+            `}>
+            {children}
         </button>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 
-import Link from "next/link";
 import ScreenLayout from "../components/ScreenLayout";
 import Button from "./components/Button";
+import Image from "next/image";
 
 const quickLinks = [
   { href: "/auth", label: "Login / Register" },
@@ -14,12 +14,21 @@ const quickLinks = [
 export default function HomePage() {
   return (
     <ScreenLayout>
-      <section className="p-6 w-full max-w-3xl">
-        <h1 className="font-bold text-6xl my-10">Design and Build <br/> apps with ease</h1>
-        <p className="text-lg">An easy-to-use studio application that helps you to quickly and easily build systems/apps for <b>CC: Tweaked</b> minecraft mod.</p>
-        <Button className="w-full max-w-2xs mt-10">Download</Button>
+      {/* Main Section */}
+      <section className="flex md:flex-row-reverse flex-col justify-between items-center md:gap-12 py-2">
+        <Image className="size-[calc(200px+15vw)]" src="/images/icon.png" alt="CCraft Studio Logo" width={1000} height={1000} />
+        <div className="w-full max-w-xl">
+          <h1 className="font-bold lg:text-5xl md:text-[calc(20px+1vw)] sm:text-3xl text-2xl my-10">Design and Build <br /> apps with ease</h1>
+          <p className="md:text-lg text-md">An easy-to-use studio application that helps you to quickly and easily build systems/apps for <b className="whitespace-nowrap">CC: Tweaked</b> minecraft mod.</p>
+          <Button colors="bg-lime/85 text-shadow-lime shadow-lime" className="w-full max-w-2xs mt-12">Download</Button>
+        </div>
       </section>
-      
+
+      {/* Images Section */}
+      <section className="my-14 w-full">
+
+      </section>
+
     </ScreenLayout>
   );
 }
